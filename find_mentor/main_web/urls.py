7cc8b843bd8a11c5116a91a9762097c45e1d8423
unknown <saadan06@gmail.com>
2022-05-11 18:01:56 +0500

    before making session feature

--- find_mentor/main_web/urls.py
+++ find_mentor/main_web/urls.py
@@ -1,10 +1,16 @@
+from asyncio import tasks
 from unicodedata import name
 from django.urls import path
 from . import views
 
+
+app_name="tasks" #uniquly identifies these urls
+
 urlpatterns = [
    path("",views.index,name="index"),
-   #adding a genral url
-   path("<str:name>", views.greet,name="greet")
+   
+   #path("<str:name>", views.greet,name="greet"),  #adding a genral url
+   
+   path("task",views.task,name="tasks")
 
 ]
--- find_mentor/main_web/views.py
+++ find_mentor/main_web/views.py
@@ -1,11 +1,46 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
 
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+
+tasks = ["saad","khan","king","qwert"]
+#creating form using django
+
+class Newtaskform(forms.Form):
+   task=forms.CharField(label="new task")
+   priority=forms.IntegerField(label="pri",min_value=1,max_value=7)
+ 
 # Create your views here.
+def task(request):
+   #doing server side validation
+   if request.method == "POST":
+      form = Newtaskform(request.POST)
+      if form.is_valid():
+         new_task = form.cleaned_data["task"]
+         tasks.append(new_task)
+         return HttpResponseRedirect(reverse("tasks:tasks"))
+      else:
+         return render (request,"main_web/tasks.html",{
+            "old_form":form,
+            "tasks":tasks
+         })
+
+
+   return render(request,"main_web/tasks.html",{
+      "k":["khan","saad","lol"],
+      "tasks":tasks,
+      "forms_by_django":Newtaskform(),
+   })
+
+
 def index(request):
    #return HttpResponse("hello, world")
-   return render(request, "main_web/index.html")
+   return render(request, "main_web/index.html",{
+      
+      "tasks":tasks,
+   })
 
 def greet(request,name):
    #return HttpResponse(f"hello, {name.capitalize()} ")
