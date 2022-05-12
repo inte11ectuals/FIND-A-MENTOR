3ef3bfe09e10b2e3eefaa31e64b22c69d63c042a
unknown <saadan06@gmail.com>
2022-05-12 15:07:17 +0500

    layout(header(navbar) and footer) fully made including css

--- find_mentor/main_web/urls.py
+++ find_mentor/main_web/urls.py
@@ -4,13 +4,22 @@ from django.urls import path
 from . import views
 
 
-app_name="tasks" #uniquly identifies these urls
 
+app_name="mentor_links" #uniquly identifies these urls
 urlpatterns = [
    path("",views.index,name="index"),
    
    #path("<str:name>", views.greet,name="greet"),  #adding a genral url
    
-   path("task",views.task,name="tasks")
+   path("task",views.task,name="tasks"),
+
+   path("home",views.home,name="home"),
+   path("task",views.task,name="uni"),
+   path("task",views.task,name="mentors"),
+   path("task",views.task,name="reviews"),
+   path("task",views.task,name="about"),
+   path("task",views.task,name="signup"),
+
+
 
 ]
--- find_mentor/main_web/views.py
+++ find_mentor/main_web/views.py
@@ -5,41 +5,49 @@ from django.shortcuts import render
 from django import forms
 from django.urls import reverse
 
-tasks = ["saad","khan","king","qwert"]
+#tasks = ["saad","khan","king","qwert"]
 #creating form using django
 
 class Newtaskform(forms.Form):
    task=forms.CharField(label="new task")
-   priority=forms.IntegerField(label="pri",min_value=1,max_value=7)
+  # priority=forms.IntegerField(label="pri",min_value=1,max_value=7)
  
-# Create your views here.
+#     Create your views here.
 def task(request):
+   if "tasks" not in request.session:
+      request.session["tasks"] =[]
+
    #doing server side validation
    if request.method == "POST":
       form = Newtaskform(request.POST)
       if form.is_valid():
          new_task = form.cleaned_data["task"]
-         tasks.append(new_task)
-         return HttpResponseRedirect(reverse("tasks:tasks"))
+         #tasks.append(new_task)
+         request.session["tasks"] += [new_task]
+         return HttpResponseRedirect(reverse("mentor_links:tasks"))
       else:
          return render (request,"main_web/tasks.html",{
             "old_form":form,
-            "tasks":tasks
+            #"tasks":tasks
          })
 
 
    return render(request,"main_web/tasks.html",{
       "k":["khan","saad","lol"],
-      "tasks":tasks,
+      #"tasks":tasks,
       "forms_by_django":Newtaskform(),
+      "tasks":request.session["tasks"],
    })
 
 
 def index(request):
+   if "tasks" not in request.session:
+      request.session["tasks"] =[]
+
    #return HttpResponse("hello, world")
    return render(request, "main_web/index.html",{
       
-      "tasks":tasks,
+      "tasks":request.session["tasks"],
    })
 
 def greet(request,name):
@@ -47,5 +55,8 @@ def greet(request,name):
    return render(request,"main_web/layout.html",{
    "name":name.capitalize() ,
    })
+
+def home(request):
+   return render(request, "main_web/home.html")
 
 
